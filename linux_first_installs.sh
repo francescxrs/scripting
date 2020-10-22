b51736a93de48a39f809457bcc64b1656308f12d
francescxrs <francescreverter@hotmail.com>
2020-10-22 19:45:31 +0200

    -y en installs

--- linux_first_installs.sh
+++ linux_first_installs.sh
@@ -4,27 +4,27 @@
 sudo apt update
 
 # INSTAL·LAR EDITOR PYCHARM
-sudo apt install snap
-sudo snap install --classic pycharm-community
-sudo snap install --classic code
+sudo apt install -y snap
+sudo snap install -y --classic pycharm-community
+sudo snap install -y --classic code
 
 # INSTAL·LAR PAQUET libpq-dev. 
 # Header files for libpq5 (PostgreSQL library) and static library for compiling C programs 
 # to link with the libpq library in order to communicate with a PostgreSQL database backend.
-sudo apt install libpq-dev
+sudo apt install -y libpq-dev
 
 # INSTAL·LAR PostgreSQL. PostgreSQL is an object-relational SQL database management system.
-sudo apt install postgresql
+sudo apt install -y postgresql
 
 # INSTAL·LAR PAQUET postgresql-contrib
 # The postgresql-contrib package includes extensions and additions that are distributed along 
 # with the PostgreSQL sources, but are not (yet) officially part of the PostgreSQL core.
-sudo apt install postgresql-contrib
+sudo apt install -y postgresql-contrib
 
 # INSTAL·LAR GESTOR DE LLIBRERIES/PAQUETS pip3
 # Standard package-management system used to install and manage software packages written in
 # Python. python 3.4 and later include pip (pip3 for Python 3) by default.
-sudo apt install python3-pip
+sudo apt install -y python3-pip
 
 # INSTAL·LAR MÒDUL venv
 # The venv module provides support for creating lightweight “virtual environments” 
@@ -32,7 +32,7 @@ sudo apt install python3-pip
 # Each virtual environment has its own Python binary (which matches the version of the 
 # binary that was used to create this environment) and can have its own independent set 
 # of installed Python packages in its site directories.
-sudo apt install python3-venv
+sudo apt install -y python3-venv
 
 # INSTAL·LAR Llibreries python
 pip3 install psycopg2
