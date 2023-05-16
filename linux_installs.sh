fc787433764de44d9f4bb936eccec94f6f81fefe
francescxrs <51786740+francescxrs@users.noreply.github.com>
2023-05-16 18:00:07 +0200

    Updates and rename

--- linux_installs.sh
+++ linux_installs.sh
@@ -4,17 +4,51 @@
 nodename=$(uname -n)
 
 # Segons versió del OS (raspbian/ubuntu/...) seguirem un procés o altre. 
-if [ "${nodename}" != "raspberrypi" ]
-then 
-echo -e "\e[95m\nEntrem en instal·lació per a sistema operatiu $nodename\e[0m"
+#if [ "${nodename}" != "raspberrypi" ]
+#then
 
-# Package manager snap, tipus apt.
+echo -e "\e[95m\nEntrem en instal·lació per a sistema operatiu $nodename\e[0m"
+cd
+sudo apt-get update
 sudo apt install -y snap
+sudo apt install -y wget curl git neovim
+sudo apt install -y default-jre
+sudo apt install -y default-jdk
+sudo apt install -y python3 python3-pip
+sudo apt install -y neovim
+sudo apt install -y w3m surf qutebrowser chromium
+sudo apt install -y ranger calcurse
+sudo apt install -y unzip
+sudo apt install -y vlc mpv
+
+# SUCKLESS STUFF
+sudo apt install -y build-essential
+sudo apt install -y libx11-dev libxft-dev libxinerama-dev
+sudo apt install -y xinit
+mkdir suckless
+cd suckless
+git clone https://git.suckless.org/dwm
+cd dwm
+sudo make clean install
+cd ..
+git clone https://git.suckless.org/st
+cd st
+sudo make clean install
+cd ..
+git clone https://git.suckless.org/dmenu
+cd dmenu
+sudo make clean install
+cd
+echo -e "\e[95m\nRecorda configurar arxiu /etc/X11/xinit/xinitrc (exec dwm) !!!\e[0m"
+# Configura arxiu /etc/X11/xinit/xinitrc en mode sudo, comenta tot i deixa només "exec dwm"
+# startx per iniciar desde terminal (raspi case)
+
+# WINE STUFF
 # Afegim repositori universe de l'instal·lador apt i actualitzem apt.
 sudo add-apt-repository universe
 # Habilitar l'arquitectura de 32 bits	
 sudo dpkg --add-architecture i386
-# Importeem keys de configuracio per a wine, aprox.
+# Importem keys de configuracio per a wine, aprox.
 wget -nc https://dl.winehq.org/wine-builds/winehq.key
 sudo apt-key add winehq.key
 # Afegim repositoris wine
@@ -34,12 +68,12 @@ sudo apt install --install-recommends winehq-stable
 # Configuracio de wine
 winecfg
 
-# sudo snap install --classic pycharm-community
-# Instal·lar editor Visual Studio Code
-sudo snap install -y --classic code
-# Si volem instal.lar pycharm hauriem de ...
+# OPTIONALS
+# Instal·lar editor Visual Studio Code o PyCharm
+# sudo snap install -y --classic code
 # sudo snap install -y --classic pycharm-community
 
+# PYTHON AND DJANGO STUFF
 # Instal·lar paquet libpq-dev. 
 # Header files for libpq5 (PostgreSQL library) and static library for compiling C programs 
 # to link with the libpq library in order to communicate with a PostgreSQL database backend.
@@ -50,10 +84,6 @@ sudo apt install -y postgresql
 # The postgresql-contrib package includes extensions and additions that are distributed along 
 # with the PostgreSQL sources, but are not (yet) officially part of the PostgreSQL core.
 sudo apt install -y postgresql-contrib
-# Instal·lar gestor de llibreries/paquets pip3
-# Standard package-management system used to install and manage software packages written in
-# Python. python 3.4 and later include pip (pip3 for Python 3) by default.
-sudo apt install -y python3-pip
 # Instal·lar mòdul venv
 # The venv module provides support for creating lightweight “virtual environments” 
 # with their own site directories, optionally isolated from system site directories. 
@@ -64,26 +94,17 @@ sudo apt install -y python3-venv
 # Instal·lar Llibreries python
 pip3 install psycopg2
 pip3 install django
-# Instal·lar Java Development Kit (jdk) i Java Runtime Environment (jre) en diferents versions.
-sudo apt install -y default-jre
-sudo apt install -y default-jdk
-sudo apt install -y openjdk-8-jdk
 
-# Afegim unzip
-sudo apt install -y unzip
-# Afegim reproductor vlc
-sudo apt install -y vlc 
+#else
 
-else
-
-echo  -e "\e[95m\nEntrem en instal·lació per a sistema operatiu raspberry pi.\e[0m"
+#echo  -e "\e[95m\nEntrem en instal·lació per a sistema operatiu raspberry pi.\e[0m"
 
 # En OS Raspbian probem instal.lar code de la seguent manera.
-cd ~
+#cd ~
 # wget: non-interactive download of files from the Web >> En aquest cas un arxiu .deb
-wget https://github.com/stevedesmond-ca/vscode-arm/releases/download/1.28.2/vscode-1.28.2.deb
+#wget https://github.com/stevedesmond-ca/vscode-arm/releases/download/1.28.2/vscode-1.28.2.deb
 # Install d'un arxiu .deb desde la mateixa ruta de l'arxiu
-sudo apt install ./vscode-1.28.2.deb
+#sudo apt install ./vscode-1.28.2.deb
 
-fi
+#fi
 
