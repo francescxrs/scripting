3c9ffd9b3976fa1f0948c881d377c7892057372d
francescxrs <51786740+francescxrs@users.noreply.github.com>
2023-05-25 01:32:57 +0200

    Updates

--- linux_installs.sh
+++ linux_installs.sh
@@ -1,25 +1,31 @@
 #!/bin/bash
-
 # Utilitzarem variable per determinar si el node és raspberrypi
-nodename=$(uname -n)
-
+# nodename=$(uname -n)
 # Segons versió del OS (raspbian/ubuntu/...) seguirem un procés o altre. 
 #if [ "${nodename}" != "raspberrypi" ]
 #then
+#echo -e "\e[95m\nEntrem en instal·lació per a sistema operatiu $nodename\e[0m"
 
-echo -e "\e[95m\nEntrem en instal·lació per a sistema operatiu $nodename\e[0m"
 cd
 sudo apt-get update
-sudo apt install -y snap
+sudo apt install -y snap snapd npm
+sudo snap install core
+sudo npm install treesitter-cli
 sudo apt install -y wget curl git neovim
 sudo apt install -y default-jre default-jdk
 sudo apt install -y python3 python3-pip
+sudo apt install -y ripgrep fzf
 sudo apt install -y w3m w3m-img surf qutebrowser chromium
 sudo apt install -y ranger calcurse
 sudo apt install -y unzip
 sudo apt install -y vlc mpv
-
-# SUCKLESS STUFF
+sudo apt install -y texlive-full 
+sudo apt install -y texlive-lang-spanish texlive-lang-english texlive-lang-european 
+sudo apt install -y pandoc
+# Si no es pot espanso instal.lar tots els packages autokey
+sudo apt install espanso
+# Després recorda executar "espanso service register" i "espanso start"
+# Suckless 
 sudo apt install -y gcc build-essential
 sudo apt install -y libx11-dev libxft-dev libxinerama-dev
 sudo apt install -y xinit
@@ -40,41 +46,40 @@ cd dmenu
 make
 sudo make clean install
 cd
-echo -e "\e[95m\nRecorda configurar arxiu /etc/X11/xinit/xinitrc (exec dwm) !!!\e[0m"
 # Configura arxiu /etc/X11/xinit/xinitrc en mode sudo, comenta tot i deixa només "exec dwm"
 # startx per iniciar desde terminal (raspi case)
 
-# WINE STUFF
+# Wine stuff
 # Afegim repositori universe de l'instal·lador apt i actualitzem apt.
-sudo add-apt-repository universe
+#sudo add-apt-repository universe
 # Habilitar l'arquitectura de 32 bits	
-sudo dpkg --add-architecture i386
+#sudo dpkg --add-architecture i386
 # Importem keys de configuracio per a wine, aprox.
-wget -nc https://dl.winehq.org/wine-builds/winehq.key
-sudo apt-key add winehq.key
+#wget -nc https://dl.winehq.org/wine-builds/winehq.key
+#sudo apt-key add winehq.key
 # Afegim repositoris wine
-sudo apt-add-repository https://dl.winehq.org/wine-builds/ubuntu/
-sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ focal main'
+#sudo apt-add-repository https://dl.winehq.org/wine-builds/ubuntu/
+#sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ focal main'
 # Refresh the list of available packages and upgrade all installed packages.
-sudo apt update
+#sudo apt update
 # Instal·lem alien. Alien converts different Linux package distribution file formats to Debian and viceversa.
 # Packages: Linux Standard Base, RPM, deb, Stampede (.slp) and Slackware (tgz).
-sudo apt install -y alien
+#sudo apt install -y alien
 # Instal.lem wine en la seva versio mes estable
-sudo apt install --install-recommends winehq-stable
+#sudo apt install --install-recommends winehq-stable
 # Comprovem info de wine
 # wine --version
 # Comprovem: LSB (Linux Standard Base) and distribution-specific information.
 # lsb_release -a
 # Configuracio de wine
-winecfg
+#winecfg
 
-# OPTIONALS
+# Altres
 # Instal·lar editor Visual Studio Code o PyCharm
 # sudo snap install -y --classic code
 # sudo snap install -y --classic pycharm-community
 
-# PYTHON AND DJANGO STUFF
+# Python & Django
 # Instal·lar paquet libpq-dev. 
 # Header files for libpq5 (PostgreSQL library) and static library for compiling C programs 
 # to link with the libpq library in order to communicate with a PostgreSQL database backend.
@@ -97,7 +102,6 @@ pip3 install psycopg2
 pip3 install django
 
 #else
-
 #echo  -e "\e[95m\nEntrem en instal·lació per a sistema operatiu raspberry pi.\e[0m"
 
 # En OS Raspbian probem instal.lar code de la seguent manera.
@@ -108,4 +112,3 @@ pip3 install django
 #sudo apt install ./vscode-1.28.2.deb
 
 #fi
-
