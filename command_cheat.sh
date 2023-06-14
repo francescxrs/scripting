140615486ee222c5e13321d07f14616281d1950d
francescxrs <francescxreverter@gmail.com>
2023-06-14 13:45:13 +0200

    Renaming

---
+++ command_cheat.sh
@@ -0,0 +1,35 @@
+## POSTGRE & DJANGO
+# Reiniciem servei postgresql
+#sudo service postgresql restart
+# Executar script sql postgre.
+#sudo -u postgres psql -f ruta/script.sql -d database
+# Obrim terminal en paral·lel i executem postgresql per a usuari creat
+#x-terminal-emulator -e "psql -U $DB_USER $DB"
+
+## SHELL COMMAND
+# Creem arxiu helloworld.txt
+#cat << endOfFile > helloworld.txt
+#hello world!
+#endOfFile
+# echo personalitzat
+#echo -e "\e[95m\n Hello world! \e[0m"
+# Process kill
+#pkill -15 ...
+# Reparar NTFS
+#sudo ntfsfix /dev/sda...
+# Autokey interface
+#autokey -c
+# Per inicialitzar raspi
+#sudo raspi-config
+# Procés per reinicialitzar mysqlshell
+#service mysql stop
+#sudo /opt/lampp/lampp restart
+
+## DRIVES
+# CIM Alumnes
+# https://drive.google.com/drive/folders/1iEO0HV4jXiFBjwB2CXqACvf2jtyxNNvY?usp=sharing
+# CIM Joan
+# https://drive.google.com/drive/folders/134RjyLF97mZwVgb7XXStqYWqTsjcKgug?usp=sharing
+
+## VIM
+# Add digraphs Ctrl-k + chars. Check :digraphs
